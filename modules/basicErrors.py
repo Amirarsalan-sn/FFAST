f6cb04f1591456a7950ca3eab56d311ed7e0ac1e
Amir Arsalan Sanati <amirarsalan.sanati81@gmail.com>
2026-03-02 18:30:00 +0100

    resolved issues 24, 27, 29, 30. applied better solution for issue 17

--- modules/basicErrors.py
+++ modules/basicErrors.py
@@ -466,7 +466,7 @@ def loadUI(UIHandler, env):
             self.visualRefresh(force=True, noAutoRange=True)
 
         def addPlots(self):
-            #self.slider.setMinMax(1, self.env.getMaxSize()//2+1)  # requires more thinking (what happens to current smoothings when deleting datasets)
+            self.slider.setMinMax(1, self.env.getMaxSize()//2+1)  # requires more thinking (what happens to current smoothings when deleting datasets)
 
             smoothing = self.smoothing
             shifted = self.handler.energyShiftEnabled
@@ -524,7 +524,7 @@ def loadUI(UIHandler, env):
             self.visualRefresh(force=True, noAutoRange=True)
 
         def addPlots(self):
-            #self.slider.setMinMax(1, self.env.getMaxSize()//2+1)  # requires more thinking (what happens to current smoothings when deleting datasets)
+            self.slider.setMinMax(1, self.env.getMaxSize()//2+1)  # requires more thinking (what happens to current smoothings when deleting datasets)
             # also what happens if the smoothing value is larger than the new added dataset's size
             smoothing = self.smoothing
             for data in self.getWatchedData():
